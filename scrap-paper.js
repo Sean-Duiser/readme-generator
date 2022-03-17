5b740ea8e8618e609d1af9c51d4520fddc05b8e2
Sean Duiser <duzer84@gmail.com>
2022-03-17 13:00:34 -0400

    initial commit

---
+++ scrap-paper.js
@@ -0,0 +1,60 @@
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'projectTitle',
+        },
+        {
+            type: 'input',
+            message: 'Please enter a description for your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please provide installation instructions.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please provide usage information.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Please provide contribution guidelines.',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'Please provide test instructions.',
+            name: 'tests',
+        },
+        {
+            type: 'list',
+            message: 'Please select a license for your application.',
+            name: 'license',
+            choices: [
+                {name: 'Apache', value: 'apache'},
+                {name: 'GNU', value: 'GNU'},
+                {name: 'MIT', value: ' MIT'},
+                {name: 'BSD-2', value: 'BSD-2'},
+                {name: 'BSD-3', value: 'BSD-3'},
+            ]
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username.',
+            name: 'gitHub',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address.',
+            name: 'email',
+        }
+    ])
+    .then(answers => {
+        fs.writeFile('README.md', JSON.stringify(answers, null, 2), (err) => {
+            err ? console.error(err) : console.log('success!')
+        });
+    });
